44ecf2da48750f22ae77cb0e33afa149f50efe0e
Fathi Mohammed <fathimohamed2229@gmail.com>
2026-01-10 10:06:01 +0200

    refactore toolbar component

--- shared/components/layout/Header/components/MainHeader.tsx
+++ shared/components/layout/Header/components/MainHeader.tsx
@@ -1,7 +1,7 @@
 import logo from '@/public/images/header-logo.svg';
 import { Logo } from './Logo';
 import { Search } from './Search';
-import { Toolsbar } from './Toolsbar';
+import { Toolbar } from './Toolbar';
 
 export const MainHeader = () => {
   return (
@@ -9,7 +9,7 @@ export const MainHeader = () => {
       <div className="flex items-center justify-between gap-[61px] py-[20px]">
         <Logo src={logo} alt="Logo" width={149} height={48} />
         <Search />
-        <Toolsbar />
+        <Toolbar />
       </div>
     </div>
   );
--- shared/components/layout/Header/components/Toolbar.tsx
+++ shared/components/layout/Header/components/Toolbar.tsx
@@ -3,15 +3,18 @@
 import { LanguageSwitcher } from "@/shared/components/LanguageSwitcher";
 import { CartDropdown } from "./CartDropdown";
 
-export const Toolsbar = () => {
+export const Toolbar = () => {
   return (
     <div className="flex items-center gap-[15px] sm:gap-[24px] shrink-0">
       <div className="hidden sm:block">
         <LanguageSwitcher />
       </div>
 
+      {/* TODO : Add WishlistDropdown When Authenticated */}
       {/* <WishlistDropdown /> */}
+
       <CartDropdown />
+
 
     </div>
   );

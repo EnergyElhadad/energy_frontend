8d0b01e6741a6f18f14ba8e2349223335ccfc5d7
Fathi Mohammed <fathimohamed2229@gmail.com>
2026-04-13 23:36:52 +0200

    product card and single page refactor

--- features/SingleProduct/components/ProductTabs/index.tsx
+++ features/SingleProduct/components/ProductTabs/index.tsx
@@ -2,6 +2,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shared/components/ui
 import { ReviewsTab } from './components/ReviewsTab';
 import { useLocale, useTranslations } from 'next-intl';
 import { ProductSpecification } from '../../types';
+import { Display } from '@/shared/components/layout/Display';
 
 type Props = {
   generalDescription: string;
@@ -14,21 +15,27 @@ export const ProductTabs: React.FC<Props> = ({ generalDescription = '', specific
   const locale = useLocale();
   const t = useTranslations('SingleProduct');
 
+  const activeTab = !!generalDescription ? 'general' : specifications && specifications.length > 0 ? 'specs' : 'reviews';
+
   return (
-    <Tabs defaultValue="general" style={{ direction: locale === 'ar' ? 'rtl' : 'ltr' }}>
+    <Tabs defaultValue={activeTab} style={{ direction: locale === 'ar' ? 'rtl' : 'ltr' }}>
       <TabsList className="border-Stroke min-h-10 w-full justify-start rounded-none border-b bg-white p-0">
-        <TabsTrigger
-          className="data-[state=active]:bg-primary flex-1 rounded-none border-none bg-white py-2.5 text-sm text-gray-500 shadow-none data-[state=active]:text-white md:max-w-[226px] md:min-w-[150px] md:py-3 md:text-base"
-          value="general"
-        >
-          {t('general_description')}
-        </TabsTrigger>
-        <TabsTrigger
-          className="data-[state=active]:bg-primary flex-1 rounded-none border-none bg-white py-2.5 text-sm text-gray-500 shadow-none data-[state=active]:text-white md:max-w-[226px] md:min-w-[150px] md:py-3 md:text-base"
-          value="specs"
-        >
-          {t('technical_specifications')}
-        </TabsTrigger>
+        <Display when={!!generalDescription}>
+          <TabsTrigger
+            className="data-[state=active]:bg-primary flex-1 rounded-none border-none bg-white py-2.5 text-sm text-gray-500 shadow-none data-[state=active]:text-white md:max-w-[226px] md:min-w-[150px] md:py-3 md:text-base"
+            value="general"
+          >
+            {t('general_description')}
+          </TabsTrigger>
+        </Display>
+        <Display when={specifications && specifications.length > 0}>
+          <TabsTrigger
+            className="data-[state=active]:bg-primary flex-1 rounded-none border-none bg-white py-2.5 text-sm text-gray-500 shadow-none data-[state=active]:text-white md:max-w-[226px] md:min-w-[150px] md:py-3 md:text-base"
+            value="specs"
+          >
+            {t('technical_specifications')}
+          </TabsTrigger>
+        </Display>
         <TabsTrigger
           className="data-[state=active]:bg-primary flex-1 rounded-none border-none bg-white py-2.5 text-sm text-gray-500 shadow-none data-[state=active]:text-white md:max-w-[226px] md:min-w-[150px] md:py-3 md:text-base"
           value="reviews"
@@ -36,33 +43,25 @@ export const ProductTabs: React.FC<Props> = ({ generalDescription = '', specific
           {t('reviews')} ({reviewsCount})
         </TabsTrigger>
       </TabsList>
+
       <div className="-mt-2 min-h-[200px] bg-white p-4 md:min-h-[265px] md:p-6">
-        <TabsContent value="general">
-          <p dangerouslySetInnerHTML={{ __html: generalDescription }} />
-        </TabsContent>
-        <TabsContent value="specs">
-          {specifications && specifications.length > 0 ? (
+        <Display when={!!generalDescription}>
+          <TabsContent value="general">
+            <div className="[&_li]:mb-1 [&_ul]:list-disc [&_ul]:ps-5" dangerouslySetInnerHTML={{ __html: generalDescription }} />
+          </TabsContent>
+        </Display>
+        <Display when={specifications && specifications.length > 0}>
+          <TabsContent value="specs">
             <div className="flex flex-col overflow-hidden rounded-md">
               {specifications.map((spec, index) => (
-                <div
-                  key={index}
-                  className={`flex flex-col sm:flex-row sm:items-center px-4 py-3 md:px-6 md:py-4 ${
-                    index % 2 === 0 ? 'bg-slate-50' : 'bg-white'
-                  }`}
-                >
-                  <div className="w-full sm:w-1/3 text-sm text-gray-600 md:text-base mb-1 sm:mb-0">
-                    {spec.spec_name}
-                  </div>
-                  <div className="w-full sm:w-2/3 text-sm text-gray-800 md:text-base">
-                    {spec.spec_value}
-                  </div>
+                <div key={index} className={`flex flex-col px-4 py-3 sm:flex-row sm:items-center md:px-6 md:py-4 ${index % 2 === 0 ? 'bg-slate-50' : 'bg-white'}`}>
+                  <div className="mb-1 w-full text-sm text-gray-600 sm:mb-0 sm:w-1/3 md:text-base">{spec.spec_name}</div>
+                  <div className="w-full text-sm text-gray-800 sm:w-2/3 md:text-base">{spec.spec_value}</div>
                 </div>
               ))}
             </div>
-          ) : (
-            <p className="text-sm text-gray-500 md:text-base">{t('no_specifications') || 'No specifications available'}</p>
-          )}
-        </TabsContent>
+          </TabsContent>
+        </Display>
         <TabsContent value="reviews">
           <ReviewsTab productId={productId} />
         </TabsContent>
